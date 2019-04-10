b775646feb37355f792d2345fad45b6500e3ba1a
Paul Sori <paul.sori@gmail.com>
2019-04-10 02:53:16 -0400

    dns process management

--- modules/ddns.js
+++ modules/ddns.js
@@ -16,60 +16,74 @@ const osMap = {
 };
 
 exports.setup = function (program) {
-  async function login() {
-    var info;
-    try {
-      // login
-      const loginRes = await superagent.post(apiEndpoint + '/login').set('accept', 'json').send({
-        email: program.ddns.email,
-        password: program.ddns.password
-      });
-
-      // pull in config options
-      const configRes = await superagent.get(apiEndpoint + '/account/info').set('x-access-token', loginRes.body.token).set('accept', 'json');
-      info = configRes.body;
-    } catch (err) {
-      winston.error('Login to Auto DNS Failed');
-      winston.error(err.message);
-      return;
-    }
-
-    // write config file for FRP
-    try{
-      const iniString = `[common]${eol}server_addr = ${info.ddnsAddress}${eol}server_port = ${info.ddnsPort}${eol}token = ${info.ddnsPassword}${eol}${eol}[web]${eol}type = http${eol}local_ip = 127.0.0.1${eol}custom_domains = ${info.subdomain}.${info.domain}${eol}local_port = ${program.port}`;
-      fs.writeFileSync(program.ddns.iniFile, iniString);
-    } catch(err) {
-      winston.error('Failed to write FRP ini');
-      winston.error(err.message);
-      return;
-    }
-
-    // Boot it
-    // TODO: Retry Logic !!!
-    // TODO: Retry Logic !!!
-    // TODO: Retry Logic !!!
-    try {
-      spawnedTunnel = spawn(path.join(__dirname, `../frp/${osMap[platform]}`), ['-c', program.ddns.iniFile], {
-        // shell: true,
-        // cwd: path.join(__dirname, `../frp/`),
-        stdio: 'ignore'
-      });
-      winston.info('Auto DNS: Secure Tunnel Established');
-      winston.info(`Access Your Server At: https://${info.subdomain}.${info.domain}`);
-    }catch (err) {
-      winston.error(`Failed to boot FRP`);
-      winston.error(err.message);
-      return;
-    }
-  }
-
   if(!program.ddns || !program.ddns.email || !program.ddns.password) {
     return;
   }
 
-  login();
+  login(program);
+}
 
-  function signUp() {
+async function login(program) {
+  var info;
+  try {
+    // login
+    const loginRes = await superagent.post(apiEndpoint + '/login').set('accept', 'json').send({
+      email: program.ddns.email,
+      password: program.ddns.password
+    });
 
+    // pull in config options
+    const configRes = await superagent.get(apiEndpoint + '/account/info').set('x-access-token', loginRes.body.token).set('accept', 'json');
+    info = configRes.body;
+  } catch (err) {
+    winston.error('Login to Auto DNS Failed');
+    winston.error(err.message);
+    return;
+  }
+
+  // write config file for FRP
+  try{
+    const iniString = `[common]${eol}server_addr = ${info.ddnsAddress}${eol}server_port = ${info.ddnsPort}${eol}token = ${info.ddnsPassword}${eol}${eol}[web]${eol}type = http${eol}local_ip = 127.0.0.1${eol}custom_domains = ${info.subdomain}.${info.domain}${eol}local_port = ${program.port}`;
+    fs.writeFileSync(program.ddns.iniFile, iniString);
+  } catch(err) {
+    winston.error('Failed to write FRP ini');
+    winston.error(err.message);
+    return;
+  }
+
+  // Boot it
+  bootReverseProxy(program, info);
+}
+
+// TODO: Electron fails to kill the spawned process on exit
+// TODO: Electron fails to kill the spawned process on exit
+// TODO: Electron fails to kill the spawned process on exit
+function bootReverseProxy(program, info) {
+  if(spawnedTunnel) {
+    winston.warn('Auto DNS: Tunnel already setup');    
+  }
+
+  try {
+    spawnedTunnel = spawn(path.join(__dirname, `../frp/${osMap[platform]}`), ['-c', program.ddns.iniFile], {
+      // shell: true,
+      // cwd: path.join(__dirname, `../frp/`),
+      stdio: 'ignore'
+    });
+
+    spawnedTunnel.on('close', (code) => {
+      winston.info('Auto DNS: Tunnel Closed. Attempting to reboot');
+      setTimeout(() => { 
+        winston.info('Auto DNS: Rebooting Tunnel');
+        delete spawnedTunnel;
+        bootReverseProxy(program, info);
+      }, 4000);
+    });
+
+    winston.info('Auto DNS: Secure Tunnel Established');
+    winston.info(`Access Your Server At: https://${info.subdomain}.${info.domain}`);
+  }catch (err) {
+    winston.error(`Failed to boot FRP`);
+    winston.error(err.message);
+    return;
   }
 }
