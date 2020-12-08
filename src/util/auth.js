08a285a18710530ae7fe5d3e1cb1ac9d02b94189
IrosTheBeggar <paul.sori@pm.me>
2020-12-07 22:01:17 -0500

    new auth api cleanup

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,18 +1,18 @@
 const crypto = require('crypto');
 
-const hashConfig = {
-  hashBytes: 32,
-  saltBytes: 16,
-  iterations: 15000
-};
+const HASH_BYTES = 32;
+const SALT_BYTES = 16;
+const ITERATIONS = 15000;
+const ENCODING = 'base64';
+const ALGORITHM = 'sha512';
 
 exports.hashPassword = password => {
   return new Promise((resolve, reject) => {
-    crypto.randomBytes(hashConfig.saltBytes, (err, salt) => {
+    crypto.randomBytes(SALT_BYTES, (err, salt) => {
       if (err) { return reject('Failed to hash password'); }
-      crypto.pbkdf2(password, salt.toString('base64'), hashConfig.iterations, hashConfig.hashBytes, 'sha512', (err, hash) => {
+      crypto.pbkdf2(password, salt.toString(ENCODING), ITERATIONS, HASH_BYTES, ALGORITHM, (err, hash) => {
         if (err) { return reject('Failed to hash password'); }
-        resolve({ salt: salt.toString('base64'), hashPassword: hash.toString('base64') });
+        resolve({ salt: salt.toString(ENCODING), hashPassword: hash.toString(ENCODING) });
       });
     });
   });
@@ -20,9 +20,9 @@ exports.hashPassword = password => {
 
 exports.authenticateUser = (password, salt, givenPassword) => {
   return new Promise((resolve, reject) => {
-    crypto.pbkdf2(givenPassword, salt, hashConfig.iterations, hashConfig.hashBytes, 'sha512', (err, verifyHash) => {
+    crypto.pbkdf2(givenPassword, salt, ITERATIONS, HASH_BYTES, ALGORITHM, (err, verifyHash) => {
       if (err) { reject('Unknown Authentication Error'); }
-      if (verifyHash.toString('base64') !== password) {
+      if (verifyHash.toString(ENCODING) !== password) {
         return reject('Authentication Error: Passwords do not match');
       }
       resolve();
