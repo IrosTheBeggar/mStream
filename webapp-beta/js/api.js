d565a2a6f2f6c17c0c1196573a6ad38ce248e5da
IrosTheBeggar <paul.sori@pm.me>
2020-12-19 13:30:37 -0500

    webapp beta stuff

--- webapp-beta/js/api.js
+++ webapp-beta/js/api.js
@@ -18,6 +18,14 @@ const API = (() => {
     })
   }
 
+  module.name = () => {
+    return module.servers[module.selectedServer].name;
+  }
+
+  module.token = () => {
+    return module.servers[module.selectedServer].token;
+  }
+
   module.url = () => {
     return module.servers[module.selectedServer].url;
   }
@@ -32,7 +40,7 @@ const API = (() => {
       await axios({
         method: 'GET',
         url: `${module.url()}/api/`,
-        headers: { 'x-access-token': module.servers[0].token }
+        headers: { 'x-access-token': module.token() }
       });
     } catch (err) {
       window.location.replace(`/login?redirect=${encodeURIComponent(window.location.pathname)}`);
@@ -46,7 +54,7 @@ const API = (() => {
 
   module.axios = axios.create({
     baseURL: module.url(),
-    headers: { 'x-access-token': module.servers[0].token }
+    headers: { 'x-access-token': module.token() }
   });
 
   // TODO: We also need a way to save servers
