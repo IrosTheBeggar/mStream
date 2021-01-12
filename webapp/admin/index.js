1ed5ba93282418f3c42d9eb1edfc7d9c0c75c8a0
IrosTheBeggar <paul.sori@pm.me>
2021-01-11 22:41:00 -0500

    more admin stuff

--- webapp/admin/index.js
+++ webapp/admin/index.js
@@ -523,12 +523,16 @@ const advancedView = Vue.component('advanced-view', {
                 <table>
                   <tbody>
                     <tr>
-                      <td><b>File Uploading:</b> {{params.noUpload}}</td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td><b>File Uploading:</b> {{params.noUpload === false ? 'Enabled' : 'Disabled'}}</td>
+                      <td>
+                        [<a v-on:click="toggleFileUpload()">edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>Auth Key:</b> {{params.secret}}</td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -543,15 +547,21 @@ const advancedView = Vue.component('advanced-view', {
                   <tbody>
                     <tr>
                       <td><b>Port:</b> {{params.port}}</td>
-                      <td>[<a>info</a>][<a v-on:click="changePort()">edit</a>]</span></td>
+                      <td>
+                        [<a v-on:click="openModal('edit-port-modal')">edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>Address:</b> {{params.address}}</td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a v-on:click="openModal('edit-address-modal')">edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>SSL:</b></td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -566,19 +576,27 @@ const advancedView = Vue.component('advanced-view', {
                   <tbody>
                     <tr>
                       <td><b>Write Logs:</b></td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>Album Art:</b></td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>DB Directory:</b></td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                     <tr>
                       <td><b>Logs Directory:</b></td>
-                      <td>[<a>info</a>][<a>edit</a>]</span></td>
+                      <td>
+                        [<a>edit</a>]
+                      </td>
                     </tr>
                   </tbody>
                 </table>
@@ -590,10 +608,53 @@ const advancedView = Vue.component('advanced-view', {
     </div>
   `,
   methods: {
-    changePort: function () {
-      modVM.currentViewModal = 'edit-port-modal';
+    openModal: function(modalView) {
+      modVM.currentViewModal = modalView;
       M.Modal.getInstance(document.getElementById('admin-modal')).open();
-    }
+    },
+    toggleFileUpload: function() {
+      iziToast.question({
+        timeout: 20000,
+        close: false,
+        overlayClose: true,
+        overlay: true,
+        displayMode: 'once',
+        id: 'question',
+        zindex: 99999,
+        layout: 2,
+        maxWidth: 600,
+        title: `${this.params.noUpload === false ? 'Disable' : 'Enable'} File Uploading?`,
+        position: 'center',
+        buttons: [
+          [`<button><b>${this.params.noUpload === false ? 'Disable' : 'Enable'}</b></button>`, (instance, toast) => {
+            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+            API.axios({
+              method: 'POST',
+              url: `${API.url()}/api/v1/admin/config/noupload`,
+              data: { noUpload: !this.params.noUpload }
+            }).then(() => {
+              // update fronted data
+              Vue.set(ADMINDATA.serverParams, 'noUpload', !this.params.noUpload);
+
+              iziToast.success({
+                title: 'Updated Successfully',
+                position: 'topCenter',
+                timeout: 3500
+              });
+            }).catch(() => {
+              iziToast.error({
+                title: 'Failed',
+                position: 'topCenter',
+                timeout: 3500
+              });
+            });
+          }, true],
+          ['<button>Go Back</button>', (instance, toast) => {
+            instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+          }],
+        ]
+      });
+    },
   }
 });
 
@@ -721,6 +782,57 @@ const rpnView = Vue.component('rpn-view', {
     </div>`
 });
 
+const infoView = Vue.component('info-view', {
+  data() {
+    return {
+
+    };
+  },
+  template: `
+    <div class="container">
+      <div class="row">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 612 153" xml:space="preserve"><style>.st0,.st1{fill-rule:evenodd;clip-rule:evenodd;fill:#264679}.st1{fill:#6684b2}</style><path class="st0" d="M179.9 45.5c-6.2 0-11.5 1.7-15.9 5s-6.5 8.1-6.5 14.4c0 4.9 1.3 9.1 3.8 12.4 2.5 3.4 5.7 5.8 9.3 7.3 3.7 1.5 7.3 2.8 11 3.8s6.8 2.3 9.3 3.9c2.5 1.5 3.8 3.5 3.8 5.8 0 4.8-4.4 7.2-13.1 7.2h-24.1V118h24.1c17.1 0 25.6-6.7 25.6-20.2 0-1.9-.2-3.8-.6-5.8-.4-2-1.2-4-2.6-6-1.3-2.1-3.3-3.7-5.8-4.9-2.5-1.2-6.4-2.7-11.5-4.5l-8.8-3.1c-.7-.2-1.7-.7-2.9-1.3-1.3-.7-2.2-1.3-2.8-1.9-.6-.6-1.1-1.4-1.6-2.3-.5-.9-.7-2-.7-3.2 0-2 1-3.5 2.9-4.6 1.9-1.1 4.3-1.6 7-1.6h24.6V45.5h-24.5zM226.4 58.3v31c0 10.2 2.5 17.6 7.6 22 5.1 4.4 13 6.6 23.7 6.6v-12.8c-2.7 0-4.9-.2-6.8-.4-1.8-.3-3.7-.9-5.8-1.9-2-.9-3.6-2.6-4.7-4.9-1.1-2.3-1.6-5.2-1.6-8.7V58.3h18.8V45.5h-18.8V31.6L214 58.3h12.4zM281.1 118V76.8c0-7.2.9-12 2.6-14.5 1-1.3 2.2-2.2 3.6-2.8 1.4-.6 2.6-1 3.6-1.1 1-.1 2.5-.1 4.3-.1H310V45.5h-12.2c-3.6 0-6.5.1-8.6.3-2.1.2-4.5.9-7.3 2s-5.1 2.8-7.1 5c-4 4.4-6 12.4-6 24V118h12.3zM326.2 53.8c-6.2 7.4-9.3 17-9.3 28.9 0 10.7 3.2 19.4 9.5 26.2s14.7 10.1 25.3 10.1c8.7 0 16.3-2.7 22.7-8.1L366 102c-3.7 2.1-8.5 3.2-14.3 3.2-6.5 0-11.8-2.3-15.8-6.9-4-4.6-6-10.5-6-17.9 0-7 1.9-12.9 5.6-17.9 3.8-5 8.9-7.5 15.5-7.5 3.3 0 6.1.8 8.2 2.4 2.1 1.6 3.2 4 3.2 7.2 0 5-1.2 8.5-3.6 10.6-2.4 2.1-6.7 3.2-12.9 3.2h-6.7v11.7h5.7c20.3 0 30.5-8.5 30.5-25.4 0-13.6-7.9-20.7-23.7-21.5-10.8-.2-19.3 3.3-25.5 10.6zM412.3 73.2c-7.4 0-13.6 1.9-18.5 5.7-4.9 3.8-7.4 9.4-7.4 16.7 0 7.3 2.3 12.9 7 16.7 4.6 3.8 10.9 5.7 18.8 5.7h31V73.6c0-9.1-2.4-16-7.2-20.8-4.8-4.8-11.7-7.2-20.7-7.2h-22.9v12.8h22.3c10.9 0 16.4 6.1 16.4 18.2v28.7h-18.4c-9.1 0-13.6-3.2-13.6-9.8 0-3.3 1.2-5.9 3.6-7.8 2.4-1.8 5.8-2.7 10.2-2.7 5.1 0 9.4 1.4 12.9 4.3v-14c-4.9-1.4-9.3-2.1-13.5-2.1zM458.8 118H471V58.3h24.4V118h12.2V58.3h5.7c6.8 0 11.3.7 13.5 2 4.3 2.5 6.5 7.7 6.5 15.5V118h12.2V75.7c0-6-.6-11.2-1.9-15.5-1.2-4.3-3.9-7.8-7.9-10.6-3.9-2.7-9.1-4.1-15.7-4.1h-61.4V118z"/><path class="st1" d="M75 118.5v-83l21 13v70z"/><path fill-rule="evenodd" clip-rule="evenodd" fill="#26477b" d="M99 118.5v-69l11.5 7 10.5-7v69z"/><path class="st1" d="M124 118.5v-70l21-13v83z"/></svg>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="card-content">
+              <span class="card-title">Developed & Designed By</span>
+              <h3>Paul Sori</h3>
+              <h4><a href="mailto:paul.sori@pm.me">paul.sori@pm.me</a></h4>
+              <blockquote>
+                <h5><b>I am currently looking for work!</b> Send me an email if you would like to hire me.</h5>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+});
+
+const transcodeView = Vue.component('transcode-view', {
+  data() {
+    return {
+
+    };
+  },
+  template: `
+    <div class="container">
+      Transcode View
+    </div>`
+});
+
+const federationView = Vue.component('transcode-view', {
+  data() {
+    return {
+
+    };
+  },
+  template: `
+    <div class="container">
+      Coming Soon
+    </div>`
+});
 
 const vm = new Vue({
   el: '#content',
@@ -729,10 +841,13 @@ const vm = new Vue({
     'users-view': usersView,
     'db-view': dbView,
     'advanced-view': advancedView,
+    'info-view': infoView,
+    'transcode-view': transcodeView,
+    'federation-view': federationView,
     'rpn-view': rpnView
   },
   data: {
-    currentViewMain: 'folders-view',
+    currentViewMain: 'info-view',
     componentKey: false
   }
 });
@@ -1133,6 +1248,68 @@ const editPortModal = Vue.component('edit-port-modal', {
   }
 });
 
+const editAddressModal = Vue.component('edit-address-modal', {
+  data() {
+    return {
+      params: ADMINDATA.dbParams,
+      submitPending: false,
+      editValue: ADMINDATA.serverParams.address
+    };
+  },
+  template: `
+    <form @submit.prevent="updateParam">
+      <div class="modal-content">
+        <h4>Server Address</h4>
+        <div class="input-field">
+          <input v-model="editValue" id="edit-server-address" required type="text">
+          <label for="edit-server-address">Server Address</label>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Go Back</a>
+        <button class="btn green waves-effect waves-light" type="submit" :disabled="submitPending === true">
+          {{submitPending === false ? 'Update' : 'Updating...'}}
+        </button>
+      </div>
+    </form>`,
+  mounted: function () {
+    M.updateTextFields();
+  },
+  methods: {
+    updateParam: async function() {
+      try {
+        this.submitPending = true;
+
+        await API.axios({
+          method: 'POST',
+          url: `${API.url()}/api/v1/admin/config/address`,
+          data: { address: this.editValue }
+        });
+
+        // update fronted data
+        Vue.set(ADMINDATA.serverParams, 'address', this.editValue);
+  
+        // close & reset the modal
+        M.Modal.getInstance(document.getElementById('admin-modal')).close();
+
+        iziToast.success({
+          title: 'Address Updated.  Server is rebooting',
+          position: 'topCenter',
+          timeout: 3500
+        });
+      } catch(err) {
+        iziToast.error({
+          title: 'Update Failed',
+          position: 'topCenter',
+          timeout: 3500
+        });
+      }finally {
+        this.submitPending = false;
+      }
+    }
+  }
+});
+
 const editMaxScanModal = Vue.component('edit-max-scans-modal', {
   data() {
     return {
@@ -1455,6 +1632,7 @@ const modVM = new Vue({
     'user-access-modal': userAccessView,
     'file-explorer-modal': fileExplorerModal,
     'edit-port-modal': editPortModal,
+    'edit-address-modal': editAddressModal,
     'edit-scan-interval-modal': editScanIntervalView,
     'edit-save-interval-modal': editSaveIntervalView,
     'edit-boot-scan-delay-modal': editBootScanView,
