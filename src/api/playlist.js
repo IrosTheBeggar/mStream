074a46e9a82deff67115488095a867199b3c47cf
IrosTheBeggar <paul.sori@pm.me>
2021-11-10 21:33:58 -0500

    update playlist api

--- src/api/playlist.js
+++ src/api/playlist.js
@@ -1,7 +1,7 @@
-const winston = require('winston');
 const Joi = require('joi');
 const config = require('../state/config');
 const db = require('../db/manager');
+const { joiValidate } = require('../util/validation');
 
 exports.setup = (mstream) => {
   // TODO: This is a legacy endpoint that should be improved
@@ -21,160 +21,114 @@ exports.setup = (mstream) => {
     });
   });
 
-  mstream.post('/api/v1/playlist/delete', async (req, res) => {
-    try {
-      const schema = Joi.object({ playlistname: Joi.string().required() });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
-    }
+  mstream.post('/api/v1/playlist/delete', (req, res) => {
+    const schema = Joi.object({ playlistname: Joi.string().required() });
+    joiValidate(schema, req.body);
 
-    try {
-      if (!db.getPlaylistCollection()) { throw new Error('DB Error'); }
+    if (!db.getPlaylistCollection()) { throw new Error('DB Error'); }
 
-      db.getPlaylistCollection().findAndRemove({
-        '$and': [
-          { 'user': { '$eq': req.user.username }},
-          { 'name': { '$eq': req.body.playlistname }}
-        ]
-      });
-
-      res.json({});
-    } catch (err) {
-      winston.error('Db Error', { stack: err });
-      res.status(500).json({ error: typeof err === 'string' ? err : 'Unknown Error' });
-    }
+    db.getPlaylistCollection().findAndRemove({
+      '$and': [
+        { 'user': { '$eq': req.user.username }},
+        { 'name': { '$eq': req.body.playlistname }}
+      ]
+    });
 
     db.saveUserDB();
+    res.json({});
   });
 
-  mstream.post('/api/v1/playlist/add-song', async (req, res) => {
-    try {
-      const schema = Joi.object({
-        song: Joi.string().required(),
-        playlist: Joi.string().required()
-      });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
-    }
-    
-    try {
-      if (!db.getPlaylistCollection()) { throw new Error('No DB'); }
-      db.getPlaylistCollection().insert({
-        name: req.body.playlist,
-        filepath: req.body.song,
-        user: req.user.username
-      });
-      res.json({ });
+  mstream.post('/api/v1/playlist/add-song', (req, res) => {
+    const schema = Joi.object({
+      song: Joi.string().required(),
+      playlist: Joi.string().required()
+    });
+    joiValidate(schema, req.body);
 
-      db.saveUserDB();
-    }catch (err) {
-      winston.error('DB Error', { stack: err });
-      res.status(500).json({ error: typeof err === 'string' ? err : 'Unknown Error' });
-    }
+    if (!db.getPlaylistCollection()) { throw new Error('No DB'); }
+    db.getPlaylistCollection().insert({
+      name: req.body.playlist,
+      filepath: req.body.song,
+      user: req.user.username
+    });
+
+    db.saveUserDB();
+    res.json({});
   });
 
-  mstream.post('/api/v1/playlist/remove-song', async (req, res) => {
-    try {
-      const schema = Joi.object({ lokiid: Joi.number().integer().required() });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
-    }
-    
-    try {
-      if (!db.getPlaylistCollection()) { throw new Error('No DB'); }
-      const result = db.getPlaylistCollection().get(req.body.lokiid);
-      if (result.user !== req.user.username) {
-        throw new Error(`User ${req.user.username} tried accessing a resource they don't have access to. Playlist Loki ID: ${req.body.lokiid}`);
-      }
+  mstream.post('/api/v1/playlist/remove-song', (req, res) => {
+    const schema = Joi.object({ lokiid: Joi.number().integer().required() });
+    joiValidate(schema, req.body);
 
-      db.getPlaylistCollection().remove(result);
-      res.json({});
-      db.saveUserDB();
-    }catch (err) {
-      winston.error('Db Error', { stack: err });
-      res.status(500).json({ error: typeof err === 'string' ? err : 'Unknown Error' });
+    if (!db.getPlaylistCollection()) { throw new Error('No DB'); }
+    const result = db.getPlaylistCollection().get(req.body.lokiid);
+    if (result.user !== req.user.username) {
+      throw new Error(`User ${req.user.username} tried accessing a resource they don't have access to. Playlist Loki ID: ${req.body.lokiid}`);
     }
+
+    db.getPlaylistCollection().remove(result);
+    db.saveUserDB();
+    res.json({});
   });
 
-  mstream.post('/api/v1/playlist/new', async (req, res) => {
-    try {
-      const schema = Joi.object({ title: Joi.string().required() });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
+  mstream.post('/api/v1/playlist/new', (req, res) => {
+    const schema = Joi.object({ title: Joi.string().required() });
+    joiValidate(schema, req.body);
+
+    const results = db.getPlaylistCollection().findOne({
+      '$and': [
+        { 'user': { '$eq': req.user.username } },
+        { 'name': { '$eq': req.body.title } }
+      ]
+    });
+
+    if (results !== null) {
+      return res.status(400).json({ error: 'Playlist Already Exists' });
     }
 
-    try {
-      const results = db.getPlaylistCollection().findOne({
-        '$and': [
-          { 'user': { '$eq': req.user.username } },
-          { 'name': { '$eq': req.body.title } }
-        ]
-      });
+    // insert null entry
+    db.getPlaylistCollection().insert({
+      name: req.body.title,
+      filepath: null,
+      user: req.user.username
+    });
 
-      if (results !== null) {
-        return res.status(400).json({ error: 'Playlist Already Exists' });
-      }
-
-      // insert null entry
-      db.getPlaylistCollection().insert({
-        name: req.body.title,
-        filepath: null,
-        user: req.user.username
-      });
-
-      res.json({});
-      db.saveUserDB();
-    } catch(err) {
-      winston.error('Db Error', { stack: err });
-      res.status(500).json({ error: typeof err === 'string' ? err : 'Unknown Error' });
-    }
+    db.saveUserDB();
+    res.json({});
   });
 
-  mstream.post('/api/v1/playlist/save', async (req, res) => {
-    try {
-      const schema = Joi.object({
-        title: Joi.string().required(),
-        songs: Joi.array().items(Joi.string())
-      });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
-    }
-    
-    try {
-      // Delete existing playlist
-      db.getPlaylistCollection().findAndRemove({
-        '$and': [
-          { 'user': { '$eq': req.user.username } },
-          { 'name': { '$eq': req.body.title } }
-        ]
-      });
+  mstream.post('/api/v1/playlist/save', (req, res) => {
+    const schema = Joi.object({
+      title: Joi.string().required(),
+      songs: Joi.array().items(Joi.string())
+    });
+    joiValidate(schema, req.body);
 
-      for (const song of req.body.songs) {
-        db.getPlaylistCollection().insert({
-          name: req.body.title,
-          filepath: song,
-          user: req.user.username
-        });
-      }
+    // Delete existing playlist
+    db.getPlaylistCollection().findAndRemove({
+      '$and': [
+        { 'user': { '$eq': req.user.username } },
+        { 'name': { '$eq': req.body.title } }
+      ]
+    });
 
-      // insert null entry
+    for (const song of req.body.songs) {
       db.getPlaylistCollection().insert({
         name: req.body.title,
-        filepath: null,
+        filepath: song,
         user: req.user.username
       });
-  
-      res.json({});
-      db.saveUserDB();
-    }catch (err) {
-      winston.error('Db Error', { stack: err });
-      res.status(500).json({ error: typeof err === 'string' ? err : 'Unknown Error' });
     }
+
+    // insert null entry
+    db.getPlaylistCollection().insert({
+      name: req.body.title,
+      filepath: null,
+      user: req.user.username
+    });
+
+    db.saveUserDB();
+    res.json({});
   });
 
   mstream.get('/api/v1/playlist/getall', (req, res) => {
