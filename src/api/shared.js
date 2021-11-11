2436db83adc6b6a03c39f27d66d3378106502289
IrosTheBeggar <paul.sori@pm.me>
2021-11-10 21:29:34 -0500

    update shared

--- src/api/shared.js
+++ src/api/shared.js
@@ -6,10 +6,12 @@ const fs = require('fs').promises;
 const Joi = require('joi');
 const config = require('../state/config');
 const db = require('../db/manager');
+const { joiValidate } = require('../util/validation');
+const WebError = require('../util/web-error');
 
 function lookupShared(playlistId) {
   const playlistItem = db.getShareCollection().findOne({ 'playlistId': playlistId });
-  if (!playlistItem) { throw new Error('Playlist Not Found'); }
+  if (!playlistItem) { throw new WebError('Playlist Not Found'); }
 
   // make sure the token is still good
   jwt.verify(playlistItem.token, config.program.secret);
@@ -33,70 +35,52 @@ exports.setupBeforeSecurity = async (mstream) => {
       return res.redirect(301, req.path.slice(0, (matchEnd[0].length)*-1) + queryString[0]);
     }
 
-    try {
-      if (!req.params.playlistId) { throw 'Validation Error' }
-      let sharePage = await fs.readFile(path.join(config.program.webAppDirectory, 'shared/index.html'), 'utf-8');
-      sharePage = sharePage.replace(
-        '<script></script>', `<script>const sharedPlaylist = ${JSON.stringify(lookupShared(req.params.playlistId))}</script>`
-      );
-      res.send(sharePage);
-    } catch (err) {
-      winston.error('share error', { stack: err })
-      return res.status(403).json({ error: 'Access Denied' });
-    }
+    if (!req.params.playlistId) { throw new WebError('Validation Error', 403); }
+    let sharePage = await fs.readFile(path.join(config.program.webAppDirectory, 'shared/index.html'), 'utf-8');
+    sharePage = sharePage.replace(
+      '<script></script>',
+      `<script>const sharedPlaylist = ${JSON.stringify(lookupShared(req.params.playlistId))}</script>`
+    );
+    res.send(sharePage);
   });
 
   mstream.get('/api/v1/shared/:playlistId', (req, res) => {
-    try {
-      if (!req.params.playlistId) { throw 'Validation Error' }
-      res.json(lookupShared(req.params.playlistId));
-    } catch (err) {
-      winston.error('share error', { stack: err })
-      return res.status(403).json({ error: 'Access Denied' });
-    }
+    if (!req.params.playlistId) { throw new WebError('Validation Error', 403); }
+    res.json(lookupShared(req.params.playlistId));
   });
 }
 
 exports.setupAfterSecurity = async (mstream) => {
-  mstream.post('/api/v1/share', async (req, res) => {
-    try {
-      const schema = Joi.object({
-        playlist: Joi.array().items(Joi.string()).required(),
-        time: Joi.number().integer().positive().optional()
-      });
-      await schema.validateAsync(req.body);
-    }catch (err) {
-      return res.status(500).json({ error: 'Validation Error' });
-    }
+  mstream.post('/api/v1/share', (req, res) => {
+    const schema = Joi.object({
+      playlist: Joi.array().items(Joi.string()).required(),
+      time: Joi.number().integer().positive().optional()
+    });
+    joiValidate(schema, req.body);
 
-    try {
-      // Setup Token Data
-      const playlistId = nanoId.nanoid(10);
+    // Setup Token Data
+    const playlistId = nanoId.nanoid(10);
 
-      const tokenData = {
-        playlistId: playlistId,
-        shareToken: true,
-        username: req.user.username
-      };
+    const tokenData = {
+      playlistId: playlistId,
+      shareToken: true,
+      username: req.user.username
+    };
 
-      const jwtOptions = {};
-      if (req.body.time) { jwtOptions.expiresIn = `${req.body.time}d`; }
-      const token = jwt.sign(tokenData, config.program.secret, jwtOptions)
+    const jwtOptions = {};
+    if (req.body.time) { jwtOptions.expiresIn = `${req.body.time}d`; }
+    const token = jwt.sign(tokenData, config.program.secret, jwtOptions)
 
-      const sharedItem = {
-        playlistId: playlistId,
-        playlist: req.body.playlist,
-        user: req.user.username,
-        expires: req.body.time ? jwt.verify(token, config.program.secret).exp : null,
-        token: token
-      };
+    const sharedItem = {
+      playlistId: playlistId,
+      playlist: req.body.playlist,
+      user: req.user.username,
+      expires: req.body.time ? jwt.verify(token, config.program.secret).exp : null,
+      token: token
+    };
 
-      db.getShareCollection().insert(sharedItem);
-      db.saveShareDB();
-      res.json(sharedItem);
-    }catch (err) {
-      winston.error('Make shared error', {stack: err})
-      res.status(500).json({ error: 'Error' });
-    }
+    db.getShareCollection().insert(sharedItem);
+    db.saveShareDB();
+    res.json(sharedItem);
   });
 }
