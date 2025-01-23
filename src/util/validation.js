8072e030fb0ae804929e7682049fca92f93a445c
Nishant Jain <nishantjain@ArcherL-MCB.local>
2025-01-23 07:18:44 -0800

    instead lets use Joi

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,7 +1,7 @@
 'use strict';
 const Joi = require('joi');
-const sanitize = require('sanitize-filename');
 
+// Function to validate input using Joi
 const joiValidate = (joiSchema, validateThis, throwErr) => {
   const { error, value } = joiSchema.validate(validateThis);
 
@@ -12,10 +12,21 @@ const joiValidate = (joiSchema, validateThis, throwErr) => {
   return { error, value };
 };
 
+// Function to sanitize filenames
 const sanitizeFilename = filename => {
-  var sanitized = sanitize(filename);
+  const filenameSchema = Joi.string()
+    .pattern(/^[a-zA-Z0-9_-]{1,100}\.[a-zA-Z0-9]{1,7}$/)
+    .required();
 
-  return sanitized;
+  // Validate the filename using the schema
+  const { error, value } = joiValidate(filenameSchema, filename);
+
+  // If validation fails, throw an error or return a sanitized version
+  if (error) {
+    throw new Error('Invalid filename');
+  }
+
+  return value;
 };
 
 module.exports = {
